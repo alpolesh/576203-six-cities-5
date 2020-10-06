ad5968742eb7445357e0a8f9c8ee61bce350cd3a
Andrei Paleshchuk <alpoleshuck@gmail.com>
2020-10-07 00:24:12 +0300

    feat: add module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainPage from "../main-page/main-page";
+import Login from "../login/login";
+import Favorites from "../favorites/favorites";
+import Room from "../room/room";
 import Proptypes from "prop-types";
 
 const App = (props) => {
   const {rentCount} = props;
 
   return (
-    <MainPage rentCount={rentCount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage rentCount={rentCount}/>
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/:id">
+          <Room />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
   );
 };
 
